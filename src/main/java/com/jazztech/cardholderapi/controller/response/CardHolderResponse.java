be870807e7fd06309e51f6a25f7875a965ac4bc4
MarceloSPTECH <marcelo.cruzjr@sptech.school>
2023-07-03 15:07:01 -0300

    Melhorado HTTPStatus de algumas exceções e o Status do portador

--- src/main/java/com/jazztech/cardholderapi/controller/response/CardHolderResponse.java
+++ src/main/java/com/jazztech/cardholderapi/controller/response/CardHolderResponse.java
@@ -1,6 +1,5 @@
 package com.jazztech.cardholderapi.controller.response;
 
-import com.jazztech.cardholderapi.utils.Status;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -8,4 +7,8 @@ import lombok.Builder;
 
 @Builder(toBuilder = true)
 public record CardHolderResponse(UUID id, Status status, BigDecimal creditLimit, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public enum Status {
+        ACTIVE,
+        INACTIVE,
+    }
 }
